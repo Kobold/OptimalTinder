e48e9a13de9357be4e8557989053ca13adf8ae31
Andy Kish <agkish@gmail.com>
2015-01-25 02:03:10 -0500

    Adding flux architecture.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,71 @@ var fs = require('fs');
 var path = require('path');
 
 var _ = require('lodash');
-var Datastore = require('nedb')
+var Fluxxor = require('fluxxor');
 var React = require('react');
 var tinder = require('tinderjs');
 
+var FluxMixin = Fluxxor.FluxMixin(React);
+var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 
-var db = new Datastore({
-  filename: path.join(require('nw.gui').App.dataPath, 'something.db'),
-  autoload: true
-});
+var client = new tinder.TinderClient();
 var secrets = require('./secrets.json');
+
+/*
+ * Flux components.
+ */
+var constants = {
+  LOAD_HISTORY: 'LOAD_HISTORY',
+  LOAD_HISTORY_SUCCESS: 'LOAD_HISTORY_SUCCESS'
+}
+
+var MatchStore = Fluxxor.createStore({
+  initialize: function() {
+    this.loading = false;
+    this.matches = [];
+
+    this.bindActions(
+      constants.LOAD_HISTORY, this.onLoadHistory,
+      constants.LOAD_HISTORY_SUCCESS, this.onLoadHistorySuccess
+    );
+  },
+
+  onLoadHistory: function() {
+    this.loading = true;
+    this.emit('change');
+  },
+
+  onLoadHistorySuccess: function(payload) {
+    this.loading = false;
+    this.matches = payload.history.matches;
+    this.emit('change');
+  },
+
+  getState: function() {
+    return {
+      loading: this.loading,
+      matches: this.matches
+    };
+  }
+});
+
+var actions = {
+  loadHistory: function(history) {
+    this.dispatch(constants.LOAD_HISTORY);
+
+    client.getHistory(function(error, data) {
+      if (error) throw error;
+      console.log('History received.');
+      this.dispatch(constants.LOAD_HISTORY_SUCCESS, {history: data});
+    }.bind(this));
+  }
+};
+
+var stores = {
+  MatchStore: new MatchStore()
+};
+
+var flux = new Fluxxor.Flux(stores, actions);
 
 
 /*
@@ -58,39 +113,45 @@ var MatchList = React.createClass({
   }
 });
 
+var Application = React.createClass({
+  mixins: [FluxMixin, StoreWatchMixin('MatchStore')],
 
-var makeOnload = function(data) {
-  return function() {
-    var displayMatches = _(data.matches)
+  getStateFromFlux: function() {
+    return this.getFlux().store('MatchStore').getState();
+  },
+
+  render: function() {
+    var displayMatches = _(this.state.matches)
       .filter(function(match) { return _.has(match, 'person'); })
       .sortBy(function(match) { return match.person.ping_time; })
       .reverse()
       .take(40)
       .value();
-    React.render(
-      <MatchList matches={displayMatches} />,
-      document.getElementById('application')
+    return (
+      <div>
+        <a onClick={this.handleLoadHistory}>Load Matches</a>
+        <MatchList matches={displayMatches} />
+      </div>
     );
-  };
-};
+  },
 
-
-
-// Get recommendations.
-var client = new tinder.TinderClient();
-client.authorize(
-  secrets.token,
-  secrets.facebook_id,
-  function() {
-    console.log('Authorized');
-    client.getHistory(function(error, data) {
-      if (error) throw error;
-      console.log('History received.');
-      console.log(data);
-
-      makeOnload(data)();
-    });
+  handleLoadHistory: function(e) {
+    e.preventDefault();
+    this.getFlux().actions.loadHistory();
   }
-);
+});
 
-
+// Authorize the client and start the app.
+onload = function() {
+  client.authorize(
+    secrets.token,
+    secrets.facebook_id,
+    function() {
+      console.log('Authorized');
+      React.render(
+        <Application flux={flux} />,
+        document.getElementById('application')
+      );
+    }
+  );
+};
